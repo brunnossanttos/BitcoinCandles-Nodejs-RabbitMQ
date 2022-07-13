904f7ecab2a635b8f4eb39e0102b861002085caa
brunnossanttos <brunossrogdgers@gmail.com>
2022-07-13 19:10:57 -0300

    básico do curso

---
+++ bitcoin-candle-app/src/components/CandleStickChart.vue
@@ -0,0 +1,60 @@
+<template>
+  <apexchart
+    type="candlestick"
+    height="500"
+    :options="chartOptions"
+    :series="series"
+  ></apexchart>
+</template>
+
+<script lang="ts">
+
+import { Options, Vue } from 'vue-class-component'
+import VueApexCharts from 'vue3-apexcharts'
+import { Prop } from 'vue-property-decorator'
+
+@Options({
+  components: {
+    apexchart: VueApexCharts
+  }
+})
+export default class CandleStickChart extends Vue {
+  @Prop()
+  candles = []
+
+  chartOptions = {
+    chart: {
+      type: 'candlestick',
+      height: 350
+    },
+    title: {
+      text: 'Bitcoin preços',
+      align: 'left',
+    },
+    xaxis: {
+      type: 'time'
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true
+      }
+    },
+    
+  }
+
+  get series() {
+    const series = [{
+      data: this.candles
+    }]
+
+    return series
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: black;
+  } 
+  
+</style>
